21f96e5503c4b1897b0c16340bb0722646b5b42a
ankit-khatkar <ankitofficial1504@gmail.com>
2026-03-19 01:28:11 +0530

    add homepage test

---
+++ eCommerce-Web/src/pages/home/HomePage.test.jsx
@@ -0,0 +1,54 @@
+import { it, expect, describe, vi, beforeEach } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Product } from "./Product";
+import { MemoryRouter } from "react-router";
+import axios from "axios";
+import userEvent from "@testing-library/user-event";
+import { HomePage } from "./HomePage";
+import { data, redirect } from "react-router";
+vi.mock("axios");
+describe("HomePage Components", () => {
+  let loadCart;
+  beforeEach(() => {
+    loadCart = vi.fn();
+    axios.get.mockImplementation(async (urlPath) => {
+      if (urlPath === "/api/products") {
+        return {
+          data: [
+            {
+              id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+              image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+              name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+              rating: {
+                stars: 4.5,
+                count: 87,
+              },
+              priceCents: 1090,
+              keywords: ["socks", "sports", "apparel"],
+            },
+            {
+              id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+              image: "images/products/intermediate-composite-basketball.jpg",
+              name: "Intermediate Size Basketball",
+              rating: {
+                stars: 4,
+                count: 127,
+              },
+              priceCents: 2095,
+              keywords: ["sports", "basketballs"],
+            },
+          ],
+        };
+      }
+    });
+  });
+  it("displays the products correct", async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />;
+      </MemoryRouter>,
+    );
+    const productContainers = await screen.findAllByTestId("product-container");
+    expect(productContainers.length).toBe(2);
+  });
+});
